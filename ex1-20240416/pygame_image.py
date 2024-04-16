20cfa03bad0d9526791a30704d4399cf827303ab
yutom <c0a2314648@edu.teu.ac.jp>
2024-04-16 17:40:30 +0900

    課題１

--- ex1-20240416/pygame_image.py
+++ ex1-20240416/pygame_image.py
@@ -28,7 +28,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             koka_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            koka_rct.move_ip((1, 0))
+            koka_rct.move_ip((2, 0))
+        if key_lst[pg.K_LEFT] == False or key_lst[pg.K_RIGHT] == False:
+            koka_rct.move_ip((-1, 0))
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0]) #練習６
